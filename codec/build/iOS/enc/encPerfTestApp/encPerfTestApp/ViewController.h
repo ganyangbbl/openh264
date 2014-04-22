9bf40716a211578e90c6c9ce1fd226ae1aa24a03
Gang Yang <ganyang@cisco.com>
2014-04-22 15:17:58 +0800

    add ios perf test app

---
+++ codec/build/iOS/enc/encPerfTestApp/encPerfTestApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  encPerfTestApp
+//
+//  Created by video.mmf on 4/22/14.
+//  Copyright (c) 2014 cisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
